ac51e9e12c2a01b69b77d78820df6a250c1fe27d
Michael Barnes <m.brandon.barnes@gmail.com>
2020-05-13 18:02:29 -0500

    Exercise 1

---
+++ MOVIEZ! Exercise 1/App_Start/RouteConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MOVIEZ_
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            /*routes.MapRoute(
+                name: "MoviesByReleaseDate",
+                url: "movies/released/{year}/{month}",
+                defaults: new { controller = "Movies", action = "ByReleaseDate" }, new { year = @"\d{4}", month = @"\d{2}" }
+                );*/
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            
+
+            //, month = UrlParameter.Optional
+            //, new {year= @"\d{4}", month = @"\d{2}"}
+        }
+    }
+}
+
+//localhost:44304/Home/Index
+
+//localhost:44304/Movies/released/2015/05
---
+++ MOVIEZ! Exercise 1/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MOVIEZ_.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string street_address { get; set; }
+        public string city_address { get; set; }
+        public string state_address { get; set; }
+        public int zip_address { get; set; }
+        public long phone_address { get; set; }
+        public string email_address { get; set; }
+
+    }
+}
