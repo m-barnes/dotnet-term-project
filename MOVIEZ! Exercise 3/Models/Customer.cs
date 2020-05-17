4e392349de4be39e5220a2f5cd9c9bffc8c59176
Michael Barnes <m.brandon.barnes@gmail.com>
2020-05-17 14:25:20 -0500

    Exercise 3

---
+++ MOVIEZ! Exercise 3/Models/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MOVIEZ_.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Street Address")]
+        public string street_address { get; set; }
+        [Required]
+
+        public string city_address { get; set; }
+        [Required]
+        [StringLength(2)]
+
+        public string state_address { get; set; }
+        [Required]
+
+        public int zip_address { get; set; }
+        [Required]
+        public long phone_address { get; set; } 
+        public string email_address { get; set; }
+        public DateTime? birthdate { get; set; }
+        public bool IsSubscribed { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        [Display(Name = "Membership")]
+        public byte MembershipTypeId { get; set; }
+
+    }
+}
