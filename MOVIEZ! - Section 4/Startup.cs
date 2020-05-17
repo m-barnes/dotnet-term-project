2f0f511ad099e9239fbf7ce141bfc57607dc610a
Michael Barnes <m.brandon.barnes@gmail.com>
2020-05-17 12:23:44 -0500

    Section 4

---
+++ MOVIEZ! - Section 4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MOVIEZ_.Startup))]
+namespace MOVIEZ_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
