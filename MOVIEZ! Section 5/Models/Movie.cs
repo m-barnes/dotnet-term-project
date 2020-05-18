51571c4831285ad5118c00568a58734a829f7fbf
Michael Barnes <m.brandon.barnes@gmail.com>
2020-05-18 11:12:04 -0500

    Section 5

---
+++ MOVIEZ! Section 5/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MOVIEZ_.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Genre")]
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
+        public int ReleaseDate { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Date Added")]
+        public DateTime DateAdded { get; set; }
+        [Display(Name = "Number in Stock")]
+        public int InStock { get; set; }
+    }
+}
