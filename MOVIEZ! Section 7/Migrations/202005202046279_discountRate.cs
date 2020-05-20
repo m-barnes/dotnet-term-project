00da61214af23b86967b7bff336a3a19c7d94353
Michael Barnes <m.brandon.barnes@gmail.com>
2020-05-20 17:19:00 -0500

    Section 7 Datatables

---
+++ MOVIEZ! Section 7/Migrations/202005202046279_discountRate.cs
@@ -0,0 +1,20 @@
+namespace MOVIEZ_.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class discountRate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "DiscountRate", c => c.Byte(nullable: false));
+            DropColumn("dbo.MembershipTypes", "Discount");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.MembershipTypes", "Discount", c => c.Byte(nullable: false));
+            DropColumn("dbo.MembershipTypes", "DiscountRate");
+        }
+    }
+}
