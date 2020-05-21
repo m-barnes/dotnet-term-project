6e261de472d77f261ed0fd756ee36d8b448dea14
Michael Barnes <m.brandon.barnes@gmail.com>
2020-05-21 15:30:14 -0500

    Exercise 6

---
+++ MOVIEZ! Exercise 6/Dtos/GenreDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MOVIEZ_.Models;
+
+namespace MOVIEZ_.Dtos
+{
+    public class GenreDto
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
