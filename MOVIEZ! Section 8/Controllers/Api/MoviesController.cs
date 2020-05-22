51e0af78bdc1960217193392672b271b29aa0e05
Michael Barnes <m.brandon.barnes@gmail.com>
2020-05-22 14:29:55 -0500

    Section 8 Authorization and Authentication

---
+++ MOVIEZ! Section 8/Controllers/Api/MoviesController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MOVIEZ_.Dtos;
+using MOVIEZ_.Models;
+using AutoMapper;
+using System.Data.Entity;
+
+namespace MOVIEZ_.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //GET return all movies in the database /api/movies
+        public IHttpActionResult GetCustomers()
+        {
+            var movieDtos = _context.Movies.Include(c => c.Genre).ToList().Select(Mapper.Map<Movie, MovieDto>);
+            
+            return Ok(movieDtos);
+        }
+
+        //GET retrieve individual movies from database using id /api/movies/id#
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+
+        //POST create new movies in database /api/movies/
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            movieDto.Id = movie.Id;
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
+        }
+
+        //PUT modify existing movies in the database using id /api/movies/id#
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            Mapper.Map(movieDto, movieInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        //DELETE delete movies in the database using id /api/movies/id#
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
+
---
+++ MOVIEZ! Section 8/Models/RoleName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MOVIEZ_.Models
+{
+    public class RoleName 
+    {
+        public const string CanManageMovies = "CanManageMovies";
+        public const string CanManageCustomers = "CanManageCustomers";
+    }
+}
